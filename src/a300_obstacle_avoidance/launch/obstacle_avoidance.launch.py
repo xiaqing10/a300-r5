84fe23b15ff4b28686a3291934392015a8ff8b33
夏青 <xiaqing01@126.com>
2026-09-22 09:39:25 +0800

    feat: complete Gazebo simulation stack

--- src/a300_obstacle_avoidance/launch/obstacle_avoidance.launch.py
+++ src/a300_obstacle_avoidance/launch/obstacle_avoidance.launch.py
@@ -1,4 +1,8 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+from launch.substitutions import PathJoinSubstitution
+
 def generate_launch_description():
-    return LaunchDescription([Node(package="a300_obstacle_avoidance",executable="a300_safety_controller",name="a300_safety_controller",output="screen",parameters=["config/obstacle.yaml"])])
+    config = PathJoinSubstitution([FindPackageShare("a300_obstacle_avoidance"), "config", "obstacle.yaml"])
+    return LaunchDescription([Node(package="a300_obstacle_avoidance", executable="a300_safety_controller", name="a300_safety_controller", output="screen", parameters=[config])])
