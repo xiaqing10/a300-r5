aa628ae44c38da61b39ef3b3091179c5d413fe08
夏青 <xiaqing01@126.com>
2026-09-22 09:44:51 +0800

    feat: add top-level simulation bringup

---
+++ src/a300_bringup/launch/simulation.launch.py
@@ -0,0 +1,16 @@
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.substitutions import FindPackageShare
+from launch.substitutions import PathJoinSubstitution
+
+def generate_launch_description():
+    return LaunchDescription([
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                PathJoinSubstitution([
+                    FindPackageShare("a300_gazebo"), "launch", "sim.launch.py"
+                ])
+            )
+        )
+    ])
